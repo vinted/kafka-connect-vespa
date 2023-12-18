af78cb2af7ba48511e03fca4efbd05b6d3aeff59
Evaldas Buinauskas <7301441+buinauskas@users.noreply.github.com>
2023-12-18 12:34:14 +0200

    Do not treat result parse exception as malformed (#19)

--- src/main/java/com/vinted/kafka/connect/vespa/feeders/VespaFeederHandler.java
+++ src/main/java/com/vinted/kafka/connect/vespa/feeders/VespaFeederHandler.java
@@ -3,6 +3,7 @@ package com.vinted.kafka.connect.vespa.feeders;
 import ai.vespa.feed.client.DocumentId;
 import ai.vespa.feed.client.OperationParseException;
 import ai.vespa.feed.client.Result;
+import ai.vespa.feed.client.ResultParseException;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.vinted.kafka.connect.vespa.VespaReporter;
 import com.vinted.kafka.connect.vespa.VespaSinkConfig;
@@ -108,10 +109,11 @@ public class VespaFeederHandler {
 
         String rootCauseString = rootCause.toString().toLowerCase();
 
-        return rootCauseString.contains("status 400")
+        return !(rootCause instanceof ResultParseException)
+                && (rootCauseString.contains("status 400")
                 || rootCauseString.contains("string field value contains illegal code point")
                 || rootCause instanceof OperationParseException
-                || rootCause instanceof JsonParseException;
+                || rootCause instanceof JsonParseException);
     }
 
     private static Result ignoredResult() {
